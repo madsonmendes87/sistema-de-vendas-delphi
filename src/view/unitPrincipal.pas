c96d9e7826e4b3ef0bff9b83be6136d8cee96495
madsonmendes87 <madsonmendes87@gmail.com>
2025-09-15 22:33:44 -0300

    primeiro commit

---
+++ src/view/unitPrincipal.pas
@@ -0,0 +1,38 @@
+unit unitPrincipal;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.Menus;
+
+type
+  TForm1 = class(TForm)
+    menuPrincipal: TMainMenu;
+    Cadastro1: TMenuItem;
+    Movimentaes1: TMenuItem;
+    Financeiro1: TMenuItem;
+    Fiscal1: TMenuItem;
+    Empresas1: TMenuItem;
+    Cidades1: TMenuItem;
+    Pessoas1: TMenuItem;
+    Subgrupodeprodutos1: TMenuItem;
+    Produtos1: TMenuItem;
+    Vendas1: TMenuItem;
+    ContasaReceber1: TMenuItem;
+    Configuraes1: TMenuItem;
+    NFCe1: TMenuItem;
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.dfm}
+
+end.
